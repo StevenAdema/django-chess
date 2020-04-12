68d0b210c82f633ae6786bfb843e84f5c596502d
stevenadema <stevenadema@gmail.com>
2020-04-12 00:39:40 -0700

    added pages

---
+++ src/chessapp/products/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    title       = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    price       = models.DecimalField(max_digits=100, decimal_places=2)
+    summary     = models.TextField(default='this is cool yo!', blank=True)
+    featured    = models.BooleanField()
