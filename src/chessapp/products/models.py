96c6335e3179cb8bfb8b2dceccf8641c5ef9e806
stevenadema <stevenadema@gmail.com>
2020-04-12 22:41:38 -0700

    add model form

--- src/chessapp/products/models.py
+++ src/chessapp/products/models.py
@@ -6,4 +6,4 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price       = models.DecimalField(max_digits=100, decimal_places=2)
     summary     = models.TextField(default='this is cool yo!', blank=True)
-    featured    = models.BooleanField()
+    featured    = models.BooleanField(default=False)
