a3e883ee72bf8327806bda055857e8938f8ae255
stevenadema <stevenadema@gmail.com>
2020-04-13 21:57:54 -0700

    add django form

--- src/chessapp/products/views.py
+++ src/chessapp/products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from .models import Product
-from .forms import ProductForm
+from .forms import RawProductForm
 
 # Create your views here.
 def product_detail_view(request):
@@ -13,12 +13,36 @@ def product_detail_view(request):
     return render(request, 'products/product_detail.html', context)
 
 def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm()
-
+    my_form = RawProductForm()
+    if request.method == 'POST':
+        my_form = RawProductForm(request.POST)
+        if my_form.is_valid():
+            print(my_form.cleaned_data)
+            Product.objects.create(**my_form.cleaned_data)
+        else:
+            print(my_form.errors)
     context = {
-        'form': form
+        'form': my_form
     }
     return render(request, 'products/product_create.html', context)
+
+# def product_create_view(request):
+#     # print(request.GET)
+#     # print(request.POST)
+#     if request.method == 'POST':
+#         my_new_title = request.POST.get('title')
+#         # Product.objeect.create(title='my_new_title'))
+#         print(my_new_title)
+#     context = {}
+#     return render(request, 'products/product_create.html', context)
+
+# def product_create_view(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProductForm() 
+
+#     context = {
+#         'form': form
+#     }
+#     return render(request, 'products/product_create.html', context)
